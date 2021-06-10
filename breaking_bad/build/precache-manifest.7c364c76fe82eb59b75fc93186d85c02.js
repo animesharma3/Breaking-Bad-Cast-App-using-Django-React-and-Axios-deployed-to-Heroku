0df3997040f22ef9302ce33c5ee5b7bcedfcb856
Animesh Sharma <animesharma3@gmail.com>
2021-06-10 10:57:01 +0530

    fixing react error

---
+++ breaking_bad/build/precache-manifest.7c364c76fe82eb59b75fc93186d85c02.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15481de0dceb2050c361424e20d7708f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57cf729ef8cde8b74b11",
+    "url": "/static/css/main.9c1a8477.chunk.css"
+  },
+  {
+    "revision": "c9413a2b0e288bcb1e85",
+    "url": "/static/js/2.68f4301c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.68f4301c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57cf729ef8cde8b74b11",
+    "url": "/static/js/main.ef2f741f.chunk.js"
+  },
+  {
+    "revision": "4aad21eb92ea52ff6838",
+    "url": "/static/js/runtime-main.9adee5b9.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
